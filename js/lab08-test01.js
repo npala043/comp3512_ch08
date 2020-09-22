9ff03cdb229717ddbf97067482a2d4faf66d4118
npala043 <npala043@mtroyal.ca>
2020-09-22 10:34:18 -0600

    tyk 1

--- js/lab08-test01.js
+++ js/lab08-test01.js
@@ -1,2 +1,8 @@
 /* add code below this */
+let bill = prompt("Enter your bill total");
+let iBill = Number(bill);
 
+const tipPercent = 0.1;
+let tip = iBill*tipPercent;
+
+console.log("For bill of $" + iBill + " the tip should be $" + tip);
